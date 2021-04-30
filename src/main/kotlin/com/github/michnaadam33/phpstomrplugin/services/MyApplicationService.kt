fd46fa30fd9969cbeff1dc3b16132b6b70e79c14
GitHub Action <action@github.com>
2021-04-30 19:26:07 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/michnaadam33/phpstomrplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.michnaadam33.phpstomrplugin.services
+
+import com.github.michnaadam33.phpstomrplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/michnaadam33/phpstomrplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/michnaadam33/phpstomrplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.michnaadam33.phpstomrplugin.services
 
+import com.github.michnaadam33.phpstomrplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
